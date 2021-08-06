fd6ca6a800cda1216b686e0145ffc9892e64ab65
lanathlor <valentin.vivier@bhc-it.com>
2021-08-06 23:56:32 +0200

    fix(): test coverage is up again

--- src/namespaces/string.ts
+++ src/namespaces/string.ts
@@ -7,7 +7,7 @@ export namespace String {
 	export const includes = (s: string, n: string) => s.includes(n)
 	export const indexOf = (s: string, n: string) => s.indexOf(n)
 	export const lastIndexOf = (s: string, n: string) => s.lastIndexOf(n)
-	export const localeCompare = (s: string, n: string, local?: string | string[], params?: object) => s.localeCompare(n, local, params)
+	export const localeCompare = (s: string, n: string, local?: string | string[], params?: object) => s.localeCompare(n, local, params) === 1
 	export const match = (s: string, n: string | RegExp) => s.match(n)
 	export const matchAll = (s: string, n: RegExp) => s.matchAll(n)
 	export const normalize = (s: string, n: "NFC" | "NFD" | "NFKC" | "NFKD") => s.normalize(n)
---
+++ test/show.test.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+require('mocha-sinon');
+
+import { show, showf, showfAsync } from '../src/index';
+
+describe('test show', function() {
+
+	beforeEach(function() {
+		this.sinon.stub(console, 'log');
+	});
+
+	it('show', function() {
+		expect(show("test")).to.eql("test");
+		expect( (console.log as any).calledOnce ).to.be.true;
+		expect( (console.log as any).calledWith('test') ).to.be.true;
+	});
+
+	it('showf', function() {
+		const test = showf((t: string) => t + "test");
+
+		expect(test("test")).to.eql("testtest");
+		expect( (console.log as any).calledTwice ).to.be.true;
+		expect( (console.log as any).calledWith('test') ).to.be.true;
+	});
+
+	it('showfAsync', async function() {
+		const test = showfAsync(async (t: string) => t + "test");
+
+		expect(await test("test")).to.eql("testtest");
+		expect( (console.log as any).calledTwice ).to.be.true;
+		expect( (console.log as any).calledWith('test') ).to.be.true;
+	});
+
+
+
+});
+
