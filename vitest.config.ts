8e62191eb33d64be48442789a9af14110bb0c5aa
lanath <valentin.vivier@bhc-it.com>
2023-08-12 14:08:31 +0200

    fix: coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,16 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		exclude: [...configDefaults.exclude, 'packages/template/*']
+		exclude: [...configDefaults.exclude, 'packages/template/*'],
+		coverage: {
+			lines: 80,
+			statements: 80,
+			functions: 80,
+			branches: 80,
+			reporter: ['lcov', 'text', 'html'],
+			skipFull: true,
+			all: true,
+			exclude: ['**/*.test.*', '**/*.js']
+		}
 	}
 })
